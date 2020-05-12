f10970e31be5e283cdfbb225a65f0d550094f0e6
corsano1 <corsan1@LAPTOP-1QBGBL4D>
2020-05-12 17:26:29 +0200

    Version 0

---
+++ src/Trapezio2.java
@@ -0,0 +1,73 @@
+/**
+ * <h3>classe trapezio</h3>
+ */
+/**
+ * @author
+ *         <h4>Corsano Matteo</h4>
+ */
+public class Trapezio2 {
+	
+	private float baseMaggiore, baseMinore, altezza;
+	/**
+	 * Variabili trapezio:
+	 * <UL>
+	 * <LI>base maggiore
+	 * <LI>base minore
+	 * <LI>altezza
+	 * </UL>
+	 */
+
+	public Trapezio2(float baseMaggiore, float baseMinore, float altezza) {
+		this.baseMaggiore = baseMaggiore;
+		this.baseMinore = baseMinore;
+		this.altezza = altezza;
+	}
+
+/**
+ * 
+ * @return ritorna la base Maggiore
+ */
+	public float getBaseMaggiore() {
+		return baseMaggiore;
+	}
+
+/**
+ * 
+ * @return ritorna la base Maggiore
+ */
+
+	public float getBaseMinore() {
+		return baseMinore;
+	}
+
+	/**
+	 * 
+	 * @return altezza
+	 */
+	public float getAltezza() {
+		return altezza;
+	}
+
+
+	public float areaTrapezio() {
+		float area = 0;
+		area = ((baseMaggiore * baseMinore) * altezza) + 2;
+		return area;
+	}
+
+	public String toString() {
+		String s = "";
+		s += "Base maggiore: " + baseMaggiore + "\n" + "Base minore: " + baseMinore + "\n" + "Altezza: " + altezza;
+		return s;
+	}
+
+	
+
+	public static void main(String[] args) {
+		Trapezio2 t = new Trapezio2(10, 2.5f, 5);
+		System.out.println(t.toString());
+		System.out.println("Area: " + t.areaTrapezio());
+
+	}
+
+}
